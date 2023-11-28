2557ed4466b5eed3f147ec63135d74d26b09b7e1
seav1 <107227866+seav1@users.noreply.github.com>
2023-11-28 11:14:38 +0800

    Update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -1,31 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-wget -qO- https://github.com/tmux/tmux/releases/download/3.2/tmux-3.2.tar.gz
-wget -qO- https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz
-wget -qO- https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz
+# Download and extract tmux, libevent, and ncurses
+wget -qO- https://github.com/tmux/tmux/releases/download/3.2/tmux-3.2.tar.gz | tar -xzvf -
+wget -qO- https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz | tar -xzvf -
+wget -qO- https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz | tar -xzvf -
 
-tar -xzvf tmux-3.2.tar.gz
-tar -xzvf libevent-2.1.11-stable.tar.gz
-tar -xzvf ncurses-6.2.tar.gz
-
-cd  libevent-2.1.11-stable
-./configure --prefix=$HOME/tmux_depend --disable-shared
+# Configure, compile, and install libevent
+cd libevent-2.1.11-stable
+./configure --prefix="$HOME/tmux_depend" --disable-shared
 make && make install
+cd ..
 
-cd
-
-cd  ncurses-6.2
-./configure --prefix=$HOME/tmux_depend
+# Configure, compile, and install ncurses
+cd ncurses-6.2
+./configure --prefix="$HOME/tmux_depend"
 make && make install
+cd ..
 
-cd
-
-cd  tmux-3.2
+# Configure, compile, and install tmux
+cd tmux-3.2
 ./configure CFLAGS="-I$HOME/tmux_depend/include -I/$HOME/tmux_depend/include/ncurses" LDFLAGS="-L/$HOME/tmux_depend/lib -L/$HOME/tmux_depend/include/ncurses -L/$HOME/tmux_depend/include"
 make
-cp tmux  $HOME/tmux_depend/bin
-export PATH=$HOME/tmux_depend/bin:$PATH
+cp tmux "$HOME/tmux_depend/bin"
+cd ..
+
+# Add tmux installation directory to PATH
+echo 'export PATH="$HOME/tmux_depend/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-
-cd
-
