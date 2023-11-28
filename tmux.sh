6d2844420b232cb59157f6546ee08d2b3deca24c
seav1 <107227866+seav1@users.noreply.github.com>
2023-11-28 10:52:17 +0800

    Add files via upload

---
+++ tmux.sh
@@ -0,0 +1,29 @@
+wget -c https://github.com/tmux/tmux/releases/download/3.2/tmux-3.2.tar.gz
+wget -c https://github.com/libevent/libevent/releases/download/release-2.1.11-stable/libevent-2.1.11-stable.tar.gz
+wget -c https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz
+
+tar -xzvf tmux-3.2.tar.gz
+tar -xzvf libevent-2.1.11-stable.tar.gz
+tar -xzvf ncurses-6.2.tar.gz
+
+cd  libevent-2.1.11-stable
+./configure --prefix=$HOME/tmux_depend --disable-shared
+make && make install
+
+cd
+
+cd  ncurses-6.2
+./configure --prefix=$HOME/tmux_depend
+make && make install
+
+cd
+
+cd  tmux-3.2
+./configure CFLAGS="-I$HOME/tmux_depend/include -I/$HOME/tmux_depend/include/ncurses" LDFLAGS="-L/$HOME/tmux_depend/lib -L/$HOME/tmux_depend/include/ncurses -L/$HOME/tmux_depend/include"
+make
+cp tmux  $HOME/tmux_depend/bin
+export PATH=$HOME/tmux_depend/bin:$PATH
+source ~/.bashrc
+
+cd
+
